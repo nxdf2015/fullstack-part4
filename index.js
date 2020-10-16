f18ccb8d1ab9946985d81d081c71f562933d9b89
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-10-16 10:50:12 +0200

    part  4 2-3

--- index.js
+++ index.js
@@ -5,24 +5,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-
-blogSchema.set('toJSON',{
-    transform: (document,returnedObject)=> {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
+const routerBlog = require("./controllers/blog")
 const password=process.env.password
 const mongoUrl = `mongodb+srv://admin:${password}@cluster0.llwdf.mongodb.net/bloglist?retryWrites=true&w=majority`
 const PORT = process.env.PORT
@@ -30,27 +14,11 @@ const PORT = process.env.PORT
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
+
+
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
+app.use("/api/blogs",routerBlog)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
